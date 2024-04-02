f45cb11397af605fd895a9b3c30b131a4b95c2eb
Lyudmila <kudaevalyudmila@gmail.com>
2024-04-02 22:45:31 +0300

    Task5

---
+++ Lession1/Task5.py
@@ -0,0 +1,12 @@
+# ТАБЛИЦА УМНОЖЕНИЯ 
+
+for i in range(2, 11):
+    for j in range(2, 6):
+        print(f"{j} * {i} = {i * j}", end="\t \t")
+    print(" ")
+
+print(" ")
+for i in range(2, 11):
+    for j in range(6, 10):
+        print(f"{j} * {i} = {i * j}", end="\t \t")
+    print(" ")
