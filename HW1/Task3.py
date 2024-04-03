cc3f65a638d675b580c582230eb7c5bb2ba38736
Lyudmila <kudaevalyudmila@gmail.com>
2024-04-03 20:32:52 +0300

    Task3

--- HW1/Task3.py
+++ HW1/Task3.py
@@ -6,3 +6,19 @@ from random import randint
 num = randint(LOWER_LIMIT, UPPER_LIMIT)"""
 
 from random import randint
+
+LOWER_LIMIT = 1
+UPPER_LIMIT = 1000
+num = randint(LOWER_LIMIT, UPPER_LIMIT)
+
+for i in range(1, 11):
+    num1 = int(input("Ведите число от 0 до 1000: "))
+    if num1 == num:
+        print("Вы отгадали!")
+        break
+    elif num > num1:
+        print(f"Ваше число меньше загаданного, осталось {10 - i} попыток:")    
+    elif num < num1:
+        print(f"Ваше число больше загаданного, осталось {10- i} попыток:") 
+if  num1 != num:
+        print("К сожалению, Вы не смогли угадать число. Попробуйте позже, вам повезет")
