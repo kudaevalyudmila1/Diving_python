8b84bdd4f55ac0557389a60c16e2120710daca4d
Lyudmila <kudaevalyudmila@gmail.com>
2024-04-24 22:01:45 +0300

    Task5

---
+++ Lession4/Task5.py
@@ -0,0 +1,16 @@
+def calculate_bonus(names: list[str], rates: list[int], percents: list[str]) -> dict:
+    """Расчет премии. """
+
+
+    calculation = {}
+    for name, rate, percent in zip(names, rates, percents):
+        percent = float(percent[:-1])
+        calculation[name] = rate * percent/100
+    return calculation
+
+
+if __name__ == '__main__':
+    names = ["Oleg", "Vladimir", "Anton"]
+    rates = [60_000, 75_000, 85_000]
+    percents = ['10.25%', '8.0%', '4.5%']
+    print(calculate_bonus(names, rates, percents))
