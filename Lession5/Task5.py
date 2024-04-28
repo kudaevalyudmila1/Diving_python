116ce039ffd599d9b5ddecee1e4b924d135d3be3
Lyudmila <kudaevalyudmila@gmail.com>
2024-04-28 17:26:33 +0300

    Task6

--- Lession5/Task5.py
+++ Lession5/Task5.py
@@ -9,7 +9,7 @@
 *Превратите решение в генераторное выражение.
 """
 print(*("FizzBuzz" if i % 3 == 0 and i % 5 == 0
-        else "Fizz" if i % 3 == 0
+        else "Fizz" if i%3 == 0
         else "Buzz" if i % 5 == 0
         else i
         for i in range(1,101)))
