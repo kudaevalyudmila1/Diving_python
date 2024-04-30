876a193f46053ddbb6900e6232a40e03390871ee
Lyudmila <kudaevalyudmila@gmail.com>
2024-04-30 22:43:48 +0300

    Task1

---
+++ Lession6/Task1.py
@@ -0,0 +1,14 @@
+"""
+Вспомните какие модули вы уже проходили на курсе. 
+Создайте файл, в котором вы импортируете встроенные в модуль функции под псевдонимами. (3-7 строк импорта).
+"""
+
+import random as r
+import sys 
+import math as m
+from collections import Counter, deque
+
+import pandas as p
+import numpy as np
+
+import Lession4.Task1
