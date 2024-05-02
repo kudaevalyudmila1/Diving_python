dfadfaa444c391ba8c859484635f3867485cb943
Lyudmila <kudaevalyudmila@gmail.com>
2024-05-02 21:04:08 +0300

    Task1

---
+++ HW6/Task1.py
@@ -0,0 +1,25 @@
+"""
+В модуль с проверкой даты добавьте возможность запуска в терминале с передачей даты на проверку.
+"""
+
+from datetime import datetime as dt
+from calendar import isleap
+from sys import argv
+
+def check_date(date: str):
+    try:
+        t = dt.strptime(date, '%d.%m.%Y')
+        _isleap(t.year)
+        return True
+    except:
+        return False
+    
+
+def _isleap(year: int):
+    print("Високосный" if isleap(year) else "Невисокосный")
+
+
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print(check_date(argv[1]))
+    print(check_date(input('Введите дату чч.мм.гггг: ')))
