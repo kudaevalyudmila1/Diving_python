b01543dd00bbb640d3e4c3a121ea3b61b03e9caf
Lyudmila <kudaevalyudmila@gmail.com>
2024-05-02 21:38:17 +0300

    init

--- main_hw.py
+++ main_hw.py
@@ -1,4 +1,4 @@
-from HW6 import Task2
+from HW6 import Task1
 
 if __name__ == '__main__':
-    Task2.queens_battle([(8, 1), (3, 6), (4, 2), (4, 5), (2, 8), (1, 7), (6, 5), (1, 5)])
+    Task1.check_date("01.01.1978")
