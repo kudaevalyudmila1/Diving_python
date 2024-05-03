4ebaea6542cd4f3bc327a8dc5d2e7fa501ffe6cd
Lyudmila <kudaevalyudmila@gmail.com>
2024-05-03 20:15:48 +0300

    Task5

---
+++ Lession7/Task5.py
@@ -0,0 +1,23 @@
+"""
+Доработаем предыдущую задачу. 
+Создайте новую функцию которая генерирует файлы с разными расширениями. 
+Расширения и количество файлов функция принимает в качестве параметров. 
+Количество переданных расширений может быть любым. 
+Количество файлов для каждого расширения различно. 
+Внутри используйте вызов функции из прошлой задачи.
+"""
+from Task4 import generate_files 
+
+def generate_with_dictionary(dictionary: dict):
+    for key, value in dictionary.items():
+        generate_files(key, 'new_files', num_files=value)
+
+
+if __name__ == '__main__':
+    d = {
+        'py': 10,
+        'jpeg': 5,
+        'txt': 11,
+        'doc': 10,
+    }
+    generate_with_dictionary(d)
