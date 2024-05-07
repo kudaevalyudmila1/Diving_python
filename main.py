b53653b37d1d1df8eba6619bd421134c737fd25f
Lyudmila <kudaevalyudmila@gmail.com>
2024-05-07 20:31:07 +0300

    Task

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from Lession6 import Task2
 
 if __name__ == '__main__':
-    Task2.guess_number(1, 2, 3)
+    generate_number_file(10, "data.txt")
---
+++ main_result_task.py
@@ -0,0 +1,4 @@
+from HW8  import Generate_files
+
+if __name__ == '__main__':
+    Generate_files.generate_files('txt', 'sub_directory')
